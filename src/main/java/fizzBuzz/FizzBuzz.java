b7abafa652afc6d75362f6729b4bfaddc638bc0e
mengmeng <mengmeng.yan@thoughtworks.com>
2018-09-24 10:28:27 +0200

    test output 1 to 100

--- src/main/java/fizzBuzz/FizzBuzz.java
+++ src/main/java/fizzBuzz/FizzBuzz.java
@@ -3,12 +3,17 @@ package fizzBuzz;
 public class FizzBuzz {
 
     public FizzBuzz() {
+        String result = "";
+        for (int i = 1; i <= 100; i++) {
+            result += decideFizzBuzz(i) + "\n";
+        }
 
+        System.out.println(result.trim());
     }
 
 
     public String decideFizzBuzz(int input) {
-        if (input == 15) {
+        if (input % 15 == 0) {
             return "FizzBuzz";
         }
         if (input % 3 == 0) {
